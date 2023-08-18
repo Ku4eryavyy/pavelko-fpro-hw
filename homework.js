f18b6c0964e1c6d3230cba12fc70a74941c5cd38
Kucheryavyy <donatellopavelko@gmail.com>
2023-08-18 23:05:31 +0200

    created homework3

---
+++ homework.js
@@ -0,0 +1,18 @@
+let firstNumber = prompt('Please enter the first number');
+let secondNumber = prompt('Please enter the second number');
+
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
+
+let add = firstNumber + secondNumber;
+let sub = firstNumber - secondNumber;
+let mult = firstNumber * secondNumber;
+let div = firstNumber / secondNumber;
+
+let result = alert(`Your result: \n
+${firstNumber} + ${secondNumber} = ${add} \n 
+${firstNumber} - ${secondNumber} = ${sub} \n
+${firstNumber} * ${secondNumber} = ${mult} \n 
+${firstNumber} / ${secondNumber} = ${div}`);
+
+console.log(`${add}\n ${sub}\n ${mult}\n ${div}`);
