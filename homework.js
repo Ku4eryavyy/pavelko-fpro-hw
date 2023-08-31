48eb3ff64c54457f83f1d5345ec07fa38d120892
Kucheryavyy <donatellopavelko@gmail.com>
2023-08-31 18:24:15 +0200

    update homework11

--- homework.js
+++ homework.js
@@ -1,13 +1,14 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
-const removeNumber = 5;
-
-const indexElement = array.indexOf(removeNumber);
-
-if (indexElement >= 0) {
-    array.splice(indexElement, 1);
-    console.log(array);
-} 
-
-else {
-    console.log('Element not found in array');
-}
+function removeNumberArray(array, removeNumber) {
+    const indexElement = array.indexOf(removeNumber);
+  
+    if (indexElement >= 0) {
+      array.splice(indexElement, 1);
+      console.log(array);
+    } else {
+      console.log('Element not found in array');
+    }
+  }
+  
+  const array = [1, 2, 3, 4, 5, 6, 7];
+  const removeNumber = 5;
+  removeNumberArray(array, removeNumber);
