546b7d89d74244713fd684db82f8a027e610021f
Kucheryavyy <donatellopavelko@gmail.com>
2023-09-07 21:44:26 +0200

    created homework14

--- homework.js
+++ homework.js
@@ -22,9 +22,8 @@
 // console.log(result);
 
 //Task 2
-
-// function doMath (x,znak,y) {
-//     switch(znak) {
+// function doMath(x, znak, y) {
+//     switch (znak) {
 //         case '*' :
 //             alert(`${x} * ${y} = ${x * y}`);
 //             break;
@@ -35,57 +34,69 @@
 //             alert(`${x} - ${y} = ${x - y}`);
 //             break;
 //         case '/':
-//             alert(`${x} / ${y} = ${x / y}`);
+//             if (y === 0) {
+//                 alert("Can't divide by 0");
+//             } else {
+//                 alert(`${x} / ${y} = ${x / y}`);
+//             }
 //             break;
 //         case '%':
-//             alert(`${x} % ${y} = ${x % y}`);
+//             if (y === 0) {
+//                 alert("Can't divide by 0");
+//             } else {
+//                 alert(`${x} % ${y} = ${x % y}`);
+//             }
 //             break;
+//         default:
+//             alert('Invalid action');
 //     }
 // }
+
 // let znak = prompt('Choose an action: *, +, -, /, %');
 
-// let x = +prompt('Please enter the first number');
-// let y = +prompt('Please enter the second number');
-// doMath(x,znak,y);
-
-function doMath(x, znak, y) {
-    switch (znak) {
-        case '*' :
-            alert(`${x} * ${y} = ${x * y}`);
-            break;
-        case '+':
-            alert(`${x} + ${y} = ${x + y}`);
-            break;
-        case '-':
-            alert(`${x} - ${y} = ${x - y}`);
-            break;
-        case '/':
-            if (y === 0) {
-                alert("Division by zero is not allowed.");
-            } else {
-                alert(`${x} / ${y} = ${x / y}`);
-            }
-            break;
-        case '%':
-            if (y === 0) {
-                alert("Modulo by zero is not allowed.");
-            } else {
-                alert(`${x} % ${y} = ${x % y}`);
-            }
-            break;
-        default:
-            alert('Invalid action');
-    }
-}
-
-let znak = prompt('Choose an action: *, +, -, /, %');
-
-if (znak === '*' || znak === '+' || znak === '-' || znak === '/' || znak === '%') {
-    let x = +prompt('Please enter the first number');
-    let y = +prompt('Please enter the second number');
+// if (znak === '*' || znak === '+' || znak === '-' || znak === '/' || znak === '%') {
+//     let x = +prompt('Please enter the first number');
+//     let y = +prompt('Please enter the second number');
     
-    doMath(x, znak, y);
-} else {
-    alert('Invalid action');
+//     doMath(x, znak, y);
+// } else {
+//     alert('Invalid action');
+// }
+
+//Task 3
+function fillingArrayData() {
+    const numRows = +prompt("Enter the number of rows for a two-world array:");
+    const numCols = +prompt("Enter the number of columns for a two-world array:");
+
+    const array = [];
+
+    for (let i = 0; i < numRows; i++) {
+        array[i] = [];
+
+        for (let j = 0; j < numCols; j++) {
+            const value = +prompt(`Enter value for element in row ${i}, column ${j}:`);
+            array[i][j] = value;
+        }
+    }
+
+    return array;
 }
 
+const userArray = fillingArrayData();
+console.log(userArray);
+
+//Task 4
+
+// function removeCharacters(inputString, charactersInput) {
+//     charactersInput.forEach(character => {
+//       inputString = inputString.split(character).join('');
+//     });
+//     return inputString;
+//   }
+  
+//   let inputString = prompt("Enter the string:");
+//   let charactersInput = prompt("Enter characters to delete (no spaces)").split('');
+//   let result = removeCharacters(inputString, charactersInput);
+//   console.log("Result: " + result);
+  
+  
