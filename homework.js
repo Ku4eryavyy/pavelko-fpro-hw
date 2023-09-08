470da246bc6a525fd617c4abbdfd947ec42ac87e
Ku4eryavyy <101946992+Ku4eryavyy@users.noreply.github.com>
2023-09-08 21:14:50 +0200

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,19 +1,17 @@
 function createSumFunction() {
-    let sum = 0;  // Змінна для накопичення суми
+    let sum = 0;
     
     function innerSum(x) {
       sum += x;
       return sum;
     }
     
-    return sum;  // Повертаємо внутрішню функцію
+    return sum;
   }
   
-  // Створюємо функцію для накопичення суми
   const sumTotal = createSumFunction();
   
-  // Використовуємо функцію замикання
-  console.log(sumTotal(3));  // Виведе 3
-  console.log(sumTotal(5));  // Виведе 8
-  console.log(sumTotal(20));  // Виведе 28
+  console.log(sumTotal(3));
+  console.log(sumTotal(5));
+  console.log(sumTotal(20));
   
