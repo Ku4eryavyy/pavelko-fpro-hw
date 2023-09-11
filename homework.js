dbb25a5039ba41d3192b442c05d152480e29d7b8
Kucheryavyy <donatellopavelko@gmail.com>
2023-09-11 22:53:52 +0200

    created homework17

---
+++ homework.js
@@ -0,0 +1,25 @@
+// let parent = document.getElementById('hover-div');
+// let newDiv;
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     parent.addEventListener('mouseover', createNewDiv);
+//     parent.addEventListener('mouseout', deleteNewDiv);
+  
+//     function createNewDiv() {
+//         newDiv = document.createElement('div');
+        
+
+//         newDiv.style.width = '100px';  
+//         newDiv.style.height = '100px';
+//         newDiv.style.backgroundColor = 'blue';
+        
+//         parent.appendChild(newDiv);
+//     }
+
+//     function deleteNewDiv() {
+//         if (newDiv) {
+//             newDiv.remove(); 
+//             newDiv = null; 
+//         }
+//     }
+// });
